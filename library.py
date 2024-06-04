5299a3f75a3c147a0b0594892f5a417261965d79
Ethan Ngawaka <eth.ecn@hotmail.com>
2024-06-04 20:55:39 +1000

    init: basic library and main with funcs

---
+++ library.py
@@ -0,0 +1,47 @@
+import pygame
+import math
+import time
+
+def test():
+    print("hello world")
+
+def init(windowW, windowH, caption):
+    pygame.init()
+    window = pygame.display.set_mode((windowW,windowH))
+    W = windowW
+    H = windowH
+    pygame.display.set_caption(caption)
+    return window
+
+def drawRect(window, rect, col): # col = (R, G, B)
+    pygame.draw.rect(window, col, pygame.Rect(*rect))
+
+def drawCircle(window, circle, col): # circle = (center, radius)
+    pygame.draw.circle(window, col, circle[0], circle[1])
+
+def AABBCollision(rect1, rect2): # rect = (x,y,w,h) returns min trans vec if true
+    checks = [
+        (rect1[0]+rect1[2] >= rect2[0]), (rect1[0] <= rect2[0]+rect2[2]),
+        (rect1[1]+rect1[3] >= rect2[1]), (rect1[1] <= rect2[1] + rect2[3])
+    ]
+
+    print((rect1[0]+rect1[2] >= rect2[0]))
+    print(rect1[0] <= rect2[0]+rect2[2])
+    print((rect1[1]+rect1[3] >= rect2[1]))
+    print((rect1[1] <= rect2[1] + rect2[3]))
+    for check in checks:
+        if not check:
+            return False
+
+    ytv21 = rect2[1] - (rect1[1]+rect1[3])
+    ytv12 = (rect2[1]+rect2[3]) - rect1[1]
+
+    xtv21 = rect2[0] - (rect1[0]+rect1[2])
+    xtv12 = (rect2[0]+rect2[2]) - rect1[0] 
+    
+    ytv = ytv21 if abs(ytv21) < abs(ytv12) else ytv12
+    xtv = xtv21 if abs(xtv21) < abs(xtv12) else xtv12
+    
+    mtv = (xtv,0) if abs(xtv) < abs(ytv) else (0,ytv)
+    return mtv
+
---
+++ main.py
@@ -0,0 +1,59 @@
+from library import *
+
+W = 800
+H = 600
+keys = [0]*512 #init keys to avoid index error (pygame has 512 keycodes) 
+# to access the state of a key (true for down false for up) use "keys[pygame.KEYCODE]"
+# eg) if keys[pygame.KEY_a]:
+#         print("a down")
+
+x = 0
+y = 0
+def update(window):
+    global keys,x,y
+    if keys[pygame.K_w]:
+        y -= 1
+    if keys[pygame.K_s]:
+        y += 1
+    if keys[pygame.K_a]:
+        x -= 1
+    if keys[pygame.K_d]:
+        x += 1
+
+    keys = pygame.key.get_pressed()
+
+def draw(window):
+    global x,y
+    drawRect(window, (0,0,W,H), (0,0,255))
+
+    temprect = (W/2, H/2, 100,100)
+    drawRect(window, temprect, (0,0,0))
+
+    check = AABBCollision((x,y,50,50), temprect)
+    print(check)
+    if check:
+        x += check[0]
+        y += check[1]
+        drawRect(window, (x,y,50,50), (255,0,0))
+    else:
+        drawRect(window, (x,y,50,50), (0,255,0))
+
+def main():
+    window = init(W,H,"poop")
+
+    running = True
+    while running:
+        update(window)
+        draw(window)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if keys[pygame.K_ESCAPE]:
+            running = False
+
+    print("Done")
+
+if __name__ == "__main__":
+    main()
