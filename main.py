d6171cd56e4adad75c4937e5aeeee793a0d700b5
Ethan Ngawaka <eth.ecn@hotmail.com>
2024-06-05 19:14:26 +1000

    feat: simple player class

--- main.py
+++ main.py
@@ -7,13 +7,29 @@ keys = [0]*512 #init keys to avoid index error (pygame has 512 keycodes)
 # eg) if keys[pygame.KEY_a]:
 #         print("a down")
 
+class Player:
+    def __init__(self,x,y):
+        w, h = 20,20
+        self.r = w/2
+        self.rect = (x,y,w,h)
+        self.col = (255, 0, 0)
+
+    def update(self, window):
+        pass
+
+    def draw(self, window):
+        drawCircle(window,((self.rect[0],self.rect[1]),self.r),self.col)
+
+player = Player(0,0)
 def update(window):
     global keys
+    player.update(window)
 
     keys = pygame.key.get_pressed()
 
 def draw(window):
     drawRect(window, (0,0,W,H), (0,0,255))
+    player.draw(window)
 
 
 def main():
