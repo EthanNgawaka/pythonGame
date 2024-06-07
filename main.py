c488f76200b7c7d6068337b3fcaf827231ac3789
Ethan Ngawaka <eth.ecn@hotmail.com>
2024-06-07 17:44:00 +1000

    feat: refactored wave spawning and shop handling

--- main.py
+++ main.py
@@ -19,42 +19,111 @@ shopManager = shopManager(W, H)
 spawnTimer = 0
 waveTimer = 1
 
-def Espawn(sc, dt, etype):
-    global waveTimer
-    if shopManager.store == False:
-        waveTimer = 1
-        shopManager.store = True
-    if waveTimer > 0:
-        global spawnTimer
-        for i in range(1):
-            if spawnTimer <= 0:
-                spawnTimer = sc
-                spawnLoc = random.randint(0,3)
-                match spawnLoc:
-                    case 0: # left
-                        enemiesOnScreen.append(etype(-player.rect[2],random.randint(0,H-player.rect[3])))
-                    case 1: # right
-                        enemiesOnScreen.append(etype(W+player.rect[2],random.randint(0,H-player.rect[3])))
-                    case 2: # up
-                        enemiesOnScreen.append(etype(random.randint(0,W-player.rect[3]), -player.rect[3]))
-                    case 3: # down
-                        enemiesOnScreen.append(etype(random.randint(0,W-player.rect[3]), H+player.rect[3]))
-            
+class Mouse:
+    def __init__(self):
+        self.pressed = [False,False]
+        self.down = [False,False]
+        self.x = 0
+        self.y = 0
+    
+    def update(self):
+        mousePos = pygame.mouse.get_pos()
+        self.x = mousePos[0]
+        self.y = mousePos[1]
+        pressed = pygame.mouse.get_pressed(num_buttons=3)
+        if pressed[0]:
+            if self.down[0]:
+                self.pressed[0] = False
+
             else:
-                spawnTimer -= dt
-                waveTimer -= dt
-                print(waveTimer)
+                self.pressed[0] = True
+            self.down[0] = True
+        else:
+            self.down[0] = False
+
+        if pressed[1]:
+            if self.down[1]:
+                self.pressed[1] = False
+
+            else:
+                self.pressed[1] = True
+            self.down[1] = True
+        else:
+            self.down[1] = False
+
+mouse = Mouse()
+
+class WaveManager:
+    def __init__(self):
+        self.spawnRate = 2
+        self.spawnTimer = 0
+        self.maxWaveTimer = 10
+        self.waveTimer = self.maxWaveTimer
+        self.swapVal = 1
+        self.wave = 1
+
+    def newWave(self):
+        self.waveTimer = self.maxWaveTimer
+        self.wave += 1
+
+    def spawnEnemy(self, enemiesOnScreen):
+        spawnLoc = random.randint(0,3)
+        enemyType = BasicEnemy # edit this at some point when we add new enemies
+        match spawnLoc:
+            case 0: # left
+                enemiesOnScreen.append(enemyType(-player.rect[2],random.randint(0,H-player.rect[3])))
+            case 1: # right
+                enemiesOnScreen.append(enemyType(W+player.rect[2],random.randint(0,H-player.rect[3])))
+            case 2: # up
+                enemiesOnScreen.append(enemyType(random.randint(0,W-player.rect[3]), -player.rect[3]))
+            case 3: # down
+                enemiesOnScreen.append(enemyType(random.randint(0,W-player.rect[3]), H+player.rect[3]))
+
+    def update(self, dt, enemiesOnScreen, shopManager, mouse, coinManager):
+        if self.waveTimer > 0:
+            self.waveTimer -= dt
+            if self.spawnTimer > 0:
+                self.spawnTimer -= dt
+            else:
+                self.spawnTimer = self.spawnRate
+                self.spawnEnemy(enemiesOnScreen)
+        elif self.swapVal == 1 and len(enemiesOnScreen) == 0 and len(coinManager.coins) == 0:
+            self.swapVal = 0
+            shopManager.store = True
+        
+        if shopManager.store:
+            shopManager.update(dt, mouse)
+            if not shopManager.store:
+                self.swapVal = 20
+
+    def draw(self, dt, window, shopManager):
+        drawText(window, f"Time left: {math.ceil(self.waveTimer)}", (255,255,255),(W-200, 50), 30) # hard coded pos shd change this
+        drawText(window, f"Wave {self.wave}", (255,255,255),(W-200, 20), 30) # hard coded pos shd change this
+        if shopManager.store and len(enemiesOnScreen) == 0:
+            shopManager.draw(window)
+        elif self.swapVal > 1:
+            self.swapVal -= 1
+            shopManager.draw(window)
+            shopManager.update(dt, mouse)
+            if self.swapVal == 1:
+                self.newWave()
+
+
+waveManager = WaveManager()
 
 def update(window, dt):
-    global keys, spawnRate, spawnTimer, enemiesOnScreen
-    player.update(window, dt, keys)
-    coinManager.update(dt, player)
+    global keys, enemiesOnScreen, mouse
+
+    if waveManager.swapVal == 1: # if not in store or transitioning from store
+        player.update(window, dt, keys)
+        coinManager.update(dt, player)
+    waveManager.update(dt, enemiesOnScreen, shopManager, mouse, coinManager)
     
     for enemy in enemiesOnScreen:
         enemy.update(window,player,dt,enemiesOnScreen,coinManager);
-        
-    Espawn(1, dt, BasicEnemy)
 
+    #input stuff
+    mouse.update()
     keys = pygame.key.get_pressed()
 
 def draw(window, dt):
@@ -63,6 +132,9 @@ def draw(window, dt):
     coinManager.draw(window)
     for enemy in enemiesOnScreen:
         enemy.draw(window);
+    waveManager.draw(dt, window, shopManager)
+    
+    drawText(window, f"FPS: {1/dt}", (255,255,255),(W-150, 150), 30)
 
 maxFPS = 60
 clock = pygame.time.Clock()
@@ -73,11 +145,6 @@ def main():
         dt = clock.tick(maxFPS) / 1000.0
         update(window, dt)
         draw(window, dt)
-        drawText(window, f"Time left: {round(waveTimer)}", (255,255,255),(W-199, 20), 30)
-        drawText(window, f"FPS: {1/dt}", (255,255,255),(W-150, 50), 30)
-        if waveTimer <= 0 and len(enemiesOnScreen) == 0:
-            shopManager.draw(window)
-            shopManager.update(dt)
 
         pygame.display.flip()
 
--- shopManager.py
+++ shopManager.py
@@ -1,76 +1,72 @@
 from library import *
-llc = True
+
+class Choice:
+    def __init__(self, choiceSpacing, name, price, col):
+        self.choiceW = 150
+        self.rect = [choiceSpacing[0], choiceSpacing[1], self.choiceW, self.choiceW]
+        self.col = col
+        self.name = name
+        self.price = price
+
+    def draw(self, window, parent):
+        pos = parent.getDrawRect(self.rect)
+        drawRect(window, pos, (155,155,155))
+        drawText(window, self.name, (255,255,255), (pos[0]+pos[2]/2,pos[1]-50), 30, True)
+        drawText(window, f"${self.price}", (255,255,255), (pos[0]+pos[2]/2,pos[1]+pos[3]+50), 30, True)
+
+    def update(self, mouse, parent):
+        pos = parent.getDrawRect(self.rect)
+        if AABBCollision(pos, [mouse.x,mouse.y,0,0]):
+            if mouse.pressed[0]:
+                print(self.name)
+                pass # run ability func from parent and delete self
+        
 class shopManager:
     def __init__(self, screenW, screenH):
         self.choiceW = 150
         backgroundW = 900
         backgroundH = 600
+        self.bgCol = (55,55,55)
 
-        self.choices = [] #choice = {pos: (int,int), name: string, price: int}
+        self.choices = [] 
         choiceSpacing = [(backgroundW - self.choiceW*3)/4, (backgroundH-self.choiceW)/2]
         
-        self.choices.append({
-            "pos": [choiceSpacing[0]*(-0.6)+self.choiceW*1, choiceSpacing[1]],
-            "name": "test1",
-            "price": random.randint(0,1000)
-        })
-        self.choices.append({
-            "pos": [choiceSpacing[0]*(0.6)+self.choiceW*2, choiceSpacing[1]],
-            "name": "test2",
-            "price": random.randint(0,1000)
-        })
-        self.choices.append({
-            "pos": [choiceSpacing[0]*(1.6)+self.choiceW*3, choiceSpacing[1]],
-            "name": "test3",
-            "price": random.randint(0,1000)
-        })
+        for i in range(3):
+            self.choices.append(Choice([choiceSpacing[0]*(i+1)+self.choiceW*i, choiceSpacing[1]], f"test{i}", random.randint(0,100), (255,255,0)))
 
         spacing = ((screenW-backgroundW)/2,(screenH-backgroundH)/2)
         self.backgroundRectTarget = [spacing[0], spacing[1], backgroundW, backgroundH]
-        self.b1 = [spacing[0], screenH + backgroundH, backgroundW, backgroundH]
         self.backgroundRect = [spacing[0], screenH + backgroundH, backgroundW, backgroundH]
-        self.backgroundRect2 = [self.backgroundRect[0], self.backgroundRect[1], 50, 50]
-        self.store = True
+        self.spawnPos = self.backgroundRect
+        self.closeButtonRect = [backgroundW-50, 0, 50, 50]
+        self.store = False
+        self.closing = False
+
+    def getDrawRect(self,offsetRect):
+        return [self.backgroundRect[0]+offsetRect[0], self.backgroundRect[1]+offsetRect[1], offsetRect[2], offsetRect[3]]
+
+    def swapStates(self):
+        if not self.store and not self.closing:
+            self.store = True
 
     def draw(self, window):
-        self.backgroundRect2[0] = self.backgroundRect[0] + 850
-        self.backgroundRect2[1] = self.backgroundRect[1]
-        drawRect(window, self.backgroundRect, (55,55,55))
-        drawRect(window, self.backgroundRect2, (255,0,0))
-        for i in self.choices:
-            pos = [self.backgroundRect[0]+i["pos"][0], self.backgroundRect[1]+i["pos"][1], self.choiceW, self.choiceW]
-            drawRect(window, pos, (155,155,155))
-            drawText(window, i["name"], (255,255,255), (pos[0]+pos[2]/2,pos[1]-50), 30, True)
-            drawText(window, f"${i['price']}", (255,255,255), (pos[0]+pos[2]/2,pos[1]+pos[3]+50), 30, True)
-            mx, my = pygame.mouse.get_pos()
-            rect1 = pos
-            rect2 = (mx,my,0,0)
-            if AABBCollision(self.backgroundRect2,rect2):
-                global llc
-                if pygame.mouse.get_pressed(num_buttons=3)[0] == True:
-                    self.store = False
+        drawRect(window, self.backgroundRect, self.bgCol)
+        closeButtonDrawRect = self.getDrawRect(self.closeButtonRect)
+        drawRect(window, closeButtonDrawRect, (255,0,0))
+        for choice in self.choices:
+            choice.draw(window, self)
 
-            if AABBCollision(rect1,rect2):
-                global llc
-                if pygame.mouse.get_pressed(num_buttons=3)[0] == True:
-                    
-                    if mx < 650 and llc == False:
-                        print("1")
-                        llc = True
-                    elif mx > 950 and llc == False:
-                        print("3")
-                        llc = True
-                    elif mx > 650 and mx < 950 and llc == False:
-                        print("2")
-                        llc = True
-                else:
-                    llc = False
-                    
-
-
-    def update(self, dt):
-        if self.store == True:
+    def update(self, dt, mouse):
+        for choice in self.choices:
+            choice.update(mouse, self)
+        if self.store:
             self.backgroundRect = rectLerp(self.backgroundRect,self.backgroundRectTarget,0.1)
         else:
-            self.backgroundRect = rectLerp(self.backgroundRect,self.b1,0.1)
+            self.backgroundRect = rectLerp(self.backgroundRect,self.spawnPos,0.1)
+
+        closeButtonDrawRect = self.getDrawRect(self.closeButtonRect)
+        if AABBCollision(closeButtonDrawRect, [mouse.x,mouse.y,0,0]):
+            if mouse.pressed[0]:
+                self.store = False
+                self.closing = True
 
